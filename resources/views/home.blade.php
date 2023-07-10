40f4da50a2bb8ff448549c032ef34a899cf5599b
ayaka <nathume.0107@gmail.com>
2023-07-11 07:48:29 +0900

    ユーザーページ作成中

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">ユーザー情報</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,17 @@
                         </div>
                     @endif
 
-                    
+                    <div class="mb-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">ユーザー名</label>
+                    <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+                    </div>
+
+                    <div class="mb-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+                    </div>
                 </div>
             </div>
         </div>
