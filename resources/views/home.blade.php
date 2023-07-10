0e36b13e4333ef3cb01ead7511983794a79d5933
ayaka <nathume.0107@gmail.com>
2023-07-10 15:21:09 +0900

    権限分け追加中

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <P>ログインしました。</p>
                 </div>
             </div>
         </div>
