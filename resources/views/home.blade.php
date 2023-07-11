260c00ec7bf32d6b8e0709ecc8e42a7c1cb6bf74
ayaka <nathume.0107@gmail.com>
2023-07-12 07:26:32 +0900

    登録項目追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,25 +14,25 @@
                         </div>
                     @endif
                     <div class="card" style="width: 6rem;">
-                        @if (!empty($user->image))
+                        @if (!empty($post->image))
                         <img src="{{ $post->image }}" class="card-img-start" alt="...">
                         @else
-                    </div>
-                        <div style="height: 50px; background-color: #e9ecef;"></div>
+                        <div style="height: 100px; background-color: #e9ecef;"></div>
                         @endif
-                        <div class="col">
-                            <label for="formGroupExampleInput2" class="form-label">ユーザー名</label>
-                            <input type="name" class="form-control" placeholder="{{ Auth::user()->name }}" aria-label="user name">
-                        </div>
-                        </div>                        
-                        <div class="mb-3">
-                            <label for="formGroupExampleInput2" class="form-label">メールアドレス</label>
-                            <input type="email" class="form-control" id="formGroupExampleInput2" placeholder="{{ Auth::user()->email }}">
-                        </div>
-                        <div class="mb-3">
-                            <label for="formGroupExampleInput2" class="form-label">パスワード</label>
-                            <input type="password" class="form-control" id="formGroupExampleInput" placeholder="{{ Auth::user()->password }}">
-                        </div>
+                    </div>
+                    <div class="col">
+                        <label for="formGroupExampleInput2" class="form-label">ユーザー名</label>
+                        <input type="text" class="form-control" placeholder="{{ Auth::user()->name }}" aria-label="user name">
+                    </div>                        
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">メールアドレス</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="{{ Auth::user()->email }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">パスワード</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="{{ Auth::user()->password }}">
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
